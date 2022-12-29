c26aee419652197c4833a940c449fab811586d24
bye9 <bye_9@naver.com>
2022-12-29 17:40:50 +0900

    1

---
+++ RPSGame/RPSGame/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  RPSGame
+//
+//  Created by JeongHwan Seok on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // 변수, 속성
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var comImageView: UIImageView!
+    @IBOutlet var myImageView: UIImageView!
+    
+    @IBOutlet var comChoiceLabel: UILabel!
+    @IBOutlet var myChoiceLabel: UILabel!
+    
+    
+    
+    
+    // 함수, 메소드
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+
+
+}
+
